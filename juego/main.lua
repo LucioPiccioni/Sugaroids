a4094e447336e03a43329b056cde54a589cb7107
LucioPiccioni <luciostefanopiccioni@gmail.com>
2024-10-01 03:07:05 -0300

    Added Random rotations to the sugaroids

--- juego/main.lua
+++ juego/main.lua
@@ -61,7 +61,7 @@ function love.draw()
     love.graphics.draw(spaceshipImage, playerPosX, playerPosY, playerAngle, playerWidth / spaceshipImage:getWidth(), playerHeight / spaceshipImage:getHeight(), playerWidth / 2, playerHeight / 2)
 
     for _, sugaroid in ipairs(sugaroids) do
-        love.graphics.draw(sugaroid.image, sugaroid.x, sugaroid.y, 0, 32 / sugaroid.image:getWidth(), 32 / sugaroid.image:getHeight())
+        love.graphics.draw(sugaroid.image, sugaroid.x, sugaroid.y, sugaroid.angle, 32 / sugaroid.image:getWidth(), 32 / sugaroid.image:getHeight(), sugaroid.image:getWidth() / 2, sugaroid.image:getHeight() / 2)
     end
 end
 
@@ -70,26 +70,27 @@ function spawnSugaroid()
     local screenHeight = love.graphics.getHeight()
     
     -- Decide desde qué borde aparecerá
-    local edge = love.math.random(1, 4)  -- 1: arriba, 2: abajo, 3: izquierda, 4: derecha
+    local edge = love.math.random(1, 4)
     local sugaroid = {
         image = sugaroidImage,
         speedX = 0,
         speedY = 0,
         x = 0,
         y = 0,
+        angle = love.math.random(0, 2 * math.pi)  -- Ángulo aleatorio en radianes
     }
 
     if edge == 1 then  -- Desde arriba
         sugaroid.x = love.math.random(0, screenWidth)
-        sugaroid.y = 0  -- Coordenada y en el borde superior
+        sugaroid.y = 0
     elseif edge == 2 then  -- Desde abajo
         sugaroid.x = love.math.random(0, screenWidth)
-        sugaroid.y = screenHeight  -- Coordenada y en el borde inferior
+        sugaroid.y = screenHeight
     elseif edge == 3 then  -- Desde la izquierda
-        sugaroid.x = 0  -- Coordenada x en el borde izquierdo
+        sugaroid.x = 0
         sugaroid.y = love.math.random(0, screenHeight)
     elseif edge == 4 then  -- Desde la derecha
-        sugaroid.x = screenWidth  -- Coordenada x en el borde derecho
+        sugaroid.x = screenWidth
         sugaroid.y = love.math.random(0, screenHeight)
     end
 
@@ -99,8 +100,8 @@ function spawnSugaroid()
     local length = math.sqrt(directionX^2 + directionY^2)
 
     if length > 0 then
-        sugaroid.speedX = (directionX / length) * 100  -- Ajusta la velocidad
-        sugaroid.speedY = (directionY / length) * 100  -- Ajusta la velocidad
+        sugaroid.speedX = (directionX / length) * 100
+        sugaroid.speedY = (directionY / length) * 100
     end
 
     table.insert(sugaroids, sugaroid)
