aeef1ed6042f2d2c5cc4acd95948d5a827c67e1c
LucioPiccioni <luciostefanopiccioni@gmail.com>
2024-10-02 05:10:15 -0300

    Changed Globals for a Global Struct Player

--- juego/main.lua
+++ juego/main.lua
@@ -1,3 +1,4 @@
+
 function love.load()
     love.window.setTitle("Sugaroids")
     
@@ -6,15 +7,15 @@ function love.load()
     sugaroidImage = love.graphics.newImage("res/sprites/enemies/sugaroid.png")
     backgroundImage = love.graphics.newImage("res/backgrounds/pacific.jfif")  
 
-    playerFriction = newFrictionJoint()
-
-    playerLives = 3
-    playerPosX = love.graphics.getWidth() / 2
-    playerPosY = love.graphics.getHeight() / 2
-    playerAngle = 0
-    playerWidth = 64 * 1.5
-    playerHeight = 64 * 1.5
-    playerSpeed = 100.0
+    player = {
+        lives = 3,
+        x = love.graphics.getWidth() / 2,
+        y = love.graphics.getHeight() / 2,
+        angle = 0,
+        width = 64 * 1.5,
+        height = 64 * 1.5,
+        speed = 100.0
+    }
 
     sugaroids = {}
     spawnTimer = 0
@@ -24,30 +25,30 @@ function love.update(dt)
     mouseX, mouseY = love.mouse.getPosition()
 
     if love.keyboard.isDown("right") then
-       if(playerPosX < love.graphics.getWidth()) then
-        playerPosX = playerPosX + playerSpeed * dt
+       if(player.x < love.graphics.getWidth()) then
+        player.x = player.x + player.speed * dt
         end
     end
     
     if love.keyboard.isDown("left") then
-        if(playerPosX > 0) then
-        playerPosX = playerPosX - playerSpeed * dt
+        if(player.x > 0) then
+            player.x = player.x - player.speed * dt
         end
     end
     
     if love.keyboard.isDown("up") then
-        if(playerPosY > 0) then
-        playerPosY = playerPosY - playerSpeed * dt
+        if(player.y > 0) then
+            player.y = player.y - player.speed * dt
         end
     end
     
     if love.keyboard.isDown("down") then
-        if(playerPosY < love.graphics.getHeight()) then
-        playerPosY = playerPosY + playerSpeed * dt
+        if(player.y < love.graphics.getHeight()) then
+            player.y = player.y + player.speed * dt
         end
     end
 
-    playerAngle = math.atan2(mouseY - playerPosY, mouseX - playerPosX)
+    player.angle = math.atan2(mouseY - player.y, mouseX - player.x)
 
     spawnTimer = spawnTimer + dt
     if spawnTimer > 1 then
@@ -70,7 +71,7 @@ end
 function love.draw()
     love.graphics.draw(backgroundImage, 0, 0)
     love.graphics.setBackgroundColor(0, 0, 0)
-    love.graphics.draw(spaceshipImage, playerPosX, playerPosY, playerAngle, playerWidth / spaceshipImage:getWidth(), playerHeight / spaceshipImage:getHeight(), spaceshipImage:getWidth() / 2, spaceshipImage:getHeight() / 2)
+    love.graphics.draw(spaceshipImage, player.x, player.y, player.angle, player.width / spaceshipImage:getWidth(), player.height / spaceshipImage:getHeight(), spaceshipImage:getWidth() / 2, spaceshipImage:getHeight() / 2)
 
     for _, sugaroid in ipairs(sugaroids) do
         love.graphics.draw(sugaroid.image, sugaroid.x, sugaroid.y, sugaroid.angle, 32 / sugaroid.image:getWidth(), 32 / sugaroid.image:getHeight(), sugaroid.image:getWidth() / 2, sugaroid.image:getHeight() / 2)
@@ -107,8 +108,8 @@ function spawnSugaroid()
     end
 
     -- Calcular la dirección hacia el jugador
-    local directionX = playerPosX - sugaroid.x
-    local directionY = playerPosY - sugaroid.y
+    local directionX = player.x - sugaroid.x
+    local directionY = player.y - sugaroid.y
     local length = math.sqrt(directionX^2 + directionY^2)
 
     if length > 0 then
